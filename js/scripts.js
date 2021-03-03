570d89d49177d00dd6664d129ee3c0094b49cb20
Bohdan Shchur <bodjashchur@gmail.com>
2021-03-03 23:40:33 +0200

    And more

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+      $("#mycarousel").carousel({interval : 2000});
+          $("#carouselButton").click(function() {
+        if($("#carouselButton").children("span").hasClass('fa-pause')){
+             $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass("fa-pause");
+            $("#carouselButton").children("span").addClass("fa-play");
+        }
+        else if($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass("fa-play");
+            $("#carouselButton").children("span").addClass("fa-pause");
+         }
+    })
+    $("#reserveButton").click(function (){
+         $("#reserveModal").modal('show');
+    });
+
+    $("#loginLink").click(function(){
+        $("#loginModal").modal('show');
+    })
+
+    $("#closeLogin").click(function(){
+        $("#loginModal").modal('hide');
+    })
+
+    $("#closeReserve").click(function(){
+        $("#reserveModal").modal('hide');
+    })
+
+    $("#cancelLogin").click(function(){
+        $("#loginModal").modal('hide');
+    })
+
+    $("#cancelReserve").click(function(){
+        $("#reserveModal").modal('hide');
+    })
+})
